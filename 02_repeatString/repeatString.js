747f6202bda961d66e0eb9193bcdd4f3bc0bfc9c
Jonny McIlroy <jonnymcilroy063@gmail.com>
2024-11-01 16:51:57 +0000

    solutions for 01_helloWorld and 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, repeats) {
+    if (repeats < 0) {
+        return 'ERROR'
+    }
+    let result = ''
+    for (let i = 1; i <= repeats; i++) {
+        result += string
+    }
+    return result
 };
 
 // Do not edit below this line
