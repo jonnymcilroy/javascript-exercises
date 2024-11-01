14cef5a619d180f31a0efff1ca555608b82056f0
Jonny McIlroy <jonnymcilroy063@gmail.com>
2024-11-01 17:33:36 +0000

    04_removeFromArray solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (arrayNumbers, ...args) {
+    results = []
+    arrayNumbers.forEach((item) => {
+        if (!args.includes(item)) {
+            results.push(item)
+        }
+    });
+    return results
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
